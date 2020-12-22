12e25f29b40c341ac617e41f98d0cb6278179c13
aneviola <b16b.v-tec.ek9@ezweb.ne.jp>
2020-12-22 22:01:07 +0900

    試験的なコミット

---
+++ pr.php
@@ -0,0 +1,40 @@
+<?php  
+
+function dif($value,$num){
+  $total = 1;
+  for ($i = $value; $i <= $num;  $i++){
+    $total = $total * $i;
+  }
+      echo $total;
+      echo  PHP_EOL;
+  }
+  dif(1,10);
+
+
+/*function sum($num){
+  $total = 0;
+  for ($i = 1; $i <= $num;  $i++){
+      $total += $i;
+  }
+      echo $total;
+      echo  PHP_EOL;
+  }
+  sum(10);
+
+function cal($x, $y){
+  return $x / $y;
+};
+$result = cal(6, 3);
+echo $result;
+
+
+class Student {
+
+  function avg($math, $english) {
+    echo (($math + $english) / 2);
+  }  
+}
+
+$a001 = new Student();
+$a001->avg(80, 70);
+*/
